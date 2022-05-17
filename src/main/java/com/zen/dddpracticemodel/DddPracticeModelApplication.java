cd992b891530bde0446b52ac1cba16a3bb4149fb
David <depch47@gmail.com>
2022-05-17 14:34:43 -0500

    starter files

---
+++ src/main/java/com/zen/dddpracticemodel/DddPracticeModelApplication.java
@@ -0,0 +1,13 @@
+package com.zen.dddpracticemodel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DddPracticeModelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DddPracticeModelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zen/dddpracticemodel/DddPracticeModelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zen.dddpracticemodel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DddPracticeModelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
