cc217a7c57c35586319ea8e6fae51c76c257f069
David <depch47@gmail.com>
2022-05-21 21:37:58 -0500

    REFACTOR

--- src/main/java/com/zen/dddpracticemodel/chef/ChefChange.java
+++ src/main/java/com/zen/dddpracticemodel/chef/ChefChange.java
@@ -1,7 +1,10 @@
 package com.zen.dddpracticemodel.chef;
 
 import co.com.sofka.domain.generic.EventChange;
+import com.zen.dddpracticemodel.chef.entities.Cook;
+import com.zen.dddpracticemodel.chef.entities.Recipe;
 import com.zen.dddpracticemodel.chef.events.*;
+import com.zen.dddpracticemodel.chef.values.IsReady;
 
 import java.util.HashSet;
 
@@ -14,26 +17,31 @@ public class ChefChange extends EventChange {
         });
 
         apply((CookAdded event ) -> {
-
+            Cook cook = new Cook(event.getCookID(), event.getName(), event.getCulinaryTool());
+            chef.cookSet.add(cook);
         });
 
         apply((CookRemoved event ) -> {
-
+            chef.cookSet.removeIf(cook -> cook.identity().equals(event.getCookID()));
         });
         apply((CookCulinaryToolUpdated event ) -> {
-
+            Cook cook1 = chef.cookSet.stream().filter(cook -> cook.identity().equals(event.getCookID())).findFirst().orElseThrow();
+            cook1.changeCulinaryTool(event.getCulinaryTool());
         });
         apply((RecipeAdded event ) -> {
-
+            Recipe recipe = new Recipe(event.getRecipeID(), event.getName(), event.getDescription());
+            chef.recipeSet.add(recipe);
         });
         apply((RecipeRemoved event ) -> {
-
+            chef.recipeSet.removeIf(recipe -> recipe.identity().equals(event.getRecipeID()));
         });
         apply((RecipeDescriptionUpdated event ) -> {
-
+            Recipe recipe = chef.recipeSet.stream().filter(recipe1 -> recipe1.identity().equals(event.getRecipeID())).findFirst().orElseThrow();
+            recipe.updateDescription(event.getDescription());
         });
         apply((RecipeIsReadyUpdated event ) -> {
-
+            Recipe recipe = chef.recipeSet.stream().filter(recipe1 -> recipe1.identity().equals(event.getRecipeID())).findFirst().orElseThrow();
+            recipe.updatedIsReady(new IsReady(!recipe.getIsReady().value()));
         });
     }
 
